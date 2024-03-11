3c5b4b5c978c17974751ab91dd9e994916851b83
kumja1 <nwajiakumj@gmail.com>
2024-03-11 21:09:31 +0000

    Corrected lang and Updated wrench.json

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,1 +1,19 @@
 //idk what u wanna put in here lol
+import {  EntityInitializationCause, world } from "@minecraft/server"
+class MechanicalBelt {
+    constructor(entity){
+
+    }
+}
+
+world.afterEvents.entitySpawn.subscribe(ev=>{
+    const {entity,cause} = ev
+   
+  if (cause === EntityInitializationCause.Spawned){
+    switch(entity.typeId){
+        case "createe:mechanical_belt": 
+        new MechanicalBelt(entity);
+        break
+    }
+  }
+})
