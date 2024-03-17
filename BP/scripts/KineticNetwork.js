577eac402d40b7e8ee072f21fcab4260bc9724b0
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2024-03-17 20:32:09 +0100

    Update KineticNetwork.js

--- BP/scripts/KineticNetwork.js
+++ BP/scripts/KineticNetwork.js
@@ -1,22 +1,38 @@
+import { world } from "@minecraft/server";
 
-class KineticNetwork {
+export default class KineticNetwork {
 
     static generateID() {
         //I hope this is random enough
         return Math.round(Math.random() * 100000000);
     }
+
+    static load(id) {
+        const data = world.getDynamicProperty(`net:${id}`);
+        if (data === undefined) {
+            return new_network = new KineticNetwork(id, new Set(), new Set());
+        };
+
+        const network_data = JSON.parse(data);
+        return new KineticNetwork(id, network_data.sources, network_data.members);
+    }
     
-    constructor() {
+    constructor(id, sources, members) {
+        this.id = id;
     
-        this.rotationalSources = new Set();
-        this.members = new Set();
+        this.sources = sources;
+        this.members = new members;
     }
 
     addMember(location) {
-        this.members.add(`x${location.x}y${location.y}${location.z}`);
+        this.members.add(`x${location.x}y${location.y}z${location.z}`);
     }
 
     addSource(location) {
-        this.rotationalSources.add(`x${location.x}y${location.y}${location.z}`);
+        this.sources.add(`x${location.x}y${location.y}z${location.z}`);
+    }
+
+    save() {
+        world.setDynamicProperty(`net:${this.id}`, JSON.stringify(this));
     }
 }
