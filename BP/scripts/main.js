e11c06cdb7b94a6e9789ab0b3993abb4fae23133
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2024-03-24 14:44:15 +0100

    Update main.js

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,4 +1,3 @@
 //idk what u wanna put in here lol
-import * as heh from "./blocks/components/Decoration"
-import * as john from "./blocks/components/TileEntity";
+import * as Components from "./blocks/components/Index";
 import * as lol from "./DebugUtils";
