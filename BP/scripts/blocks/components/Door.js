47232a7442c7779e9c763e44d8e17e1b60a037de
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2024-03-25 14:25:55 +0100

    Update Door.js

--- BP/scripts/blocks/components/Door.js
+++ BP/scripts/blocks/components/Door.js
@@ -2,22 +2,24 @@ import { world } from "@minecraft/server";
 
 world.beforeEvents.worldInitialize.subscribe(initEvent => {
     initEvent.blockTypeRegistry.registerCustomComponent('create:door', {
-        onPlace: ({ block }) => {
-            if (block.permutation.getState("create:half") === "lower") {
-                block.above().setPermutation(block.permutation.withState("create:half", 'upper'));
-            }
+        onPlace: ({ block, previousBlock }) => {
+            if (!previousBlock.hasTag("door")) {
+                if (block.permutation.getState("create:half") === "lower") {
+                    block.above().setPermutation(block.permutation.withState("create:half", 'upper'));
+                };
+            };
         },
         beforeOnPlayerPlace: e => {
             if (!e.block.above().isAir) {
                 e.cancel = true
-            }
+            };
         },
         onPlayerDestroy: ({ block, dimension }) => {
             if (block.above().hasTag("upper_door")) {
                 dimension.runCommand(`setblock ${block.location.x} ${block.location.y + 1} ${block.location.z} air destroy`);
             } else {
                 dimension.runCommand(`setblock ${block.location.x} ${block.location.y - 1} ${block.location.z} air destroy`);
-            }
+            };
         },
         onPlayerInteract: e => {
             let block2 = e.block.hasTag("upper_door") ? e.block.below() : e.block.above();
