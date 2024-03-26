94eb291e75b8e7f4775290ff0a9fa05bf4756998
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2024-03-26 12:18:36 +0100

    The hinge is now controllable on placement

--- BP/scripts/blocks/components/Door.js
+++ BP/scripts/blocks/components/Door.js
@@ -7,38 +7,32 @@ world.beforeEvents.worldInitialize.subscribe(initEvent => {
             if (!upper_block.isAir) {
                 e.cancel = true
             };
-            upper_block.setPermutation(e.permutationToPlace.withState("create:half", 'upper'));
+            const dir = e.permutationToPlace.getState("minecraft:cardinal_direction");
+            const hinge = getHinge(e.player, dir);
+            upper_block.setPermutation(e.permutationToPlace.withState("create:half", 'upper').withState("create:hinge", hinge));
+            e.permutationToPlace = e.permutationToPlace.withState("create:hinge", hinge);
+
         },
         onPlayerDestroy: e => {
             if (e.block.above().hasTag("upper_door")) {
                 e.dimension.runCommand(`setblock ${e.block.location.x} ${e.block.location.y + 1} ${e.block.location.z} air destroy`);
                 if (e.destroyedBlockPermutation.getState("create:open")) {
-                    e.dimension
-                      .getEntitiesAtBlockLocation(e.block.location)
-                      .filter((v) => v.typeId == e.destroyedBlockPermutation.type.id)[0]
-                      .remove();
-                }
+                    e.dimension.getEntitiesAtBlockLocation(e.block.location)[0].remove();
+                };
             } else {
                 e.dimension.runCommand(`setblock ${e.block.location.x} ${e.block.location.y - 1} ${e.block.location.z} air destroy`);
                 if (e.destroyedBlockPermutation.getState("create:open")) {
-                    e.dimension
-                      .getEntitiesAtBlockLocation({
-                        x: e.block.location.x,
-                        y: e.block.location.y - 1,
-                        z: e.block.location.z,
-                      })
-                      .filter((v) => v.typeId == e.destroyedBlockPermutation.type.id)[0]
-                      .remove();
-                }
+                    e.dimension.getEntitiesAtBlockLocation({x: e.block.location.x, y: e.block.location.y - 1, z: e.block.location.z})[0].remove();
+                };
             };
         },
-//Feel free to optimize it but dont remove features
         onPlayerInteract: e => {
             const block2 = e.block.hasTag("upper_door") ? e.block.below() : e.block.above();
             const direction = e.block.permutation.getState("minecraft:cardinal_direction");
             const hinge  = !e.block.hasTag("left");
             const neighbour = e.dimension.getBlock(getN(direction, e.block.location, hinge));
-            if (!e.block.hasTag("open")) {
+            const open = e.block.hasTag("open");
+            if (!open) {
                 const bottom = e.block.hasTag("upper_door") ? block2.bottomCenter() : e.block.bottomCenter();
                 const entity = e.dimension.spawnEntity(e.block.typeId, bottom);
                 entity.setProperty("create:cardinal_direction", getEntityDirection(direction));
@@ -46,13 +40,7 @@ world.beforeEvents.worldInitialize.subscribe(initEvent => {
                 e.block.setPermutation(e.block.permutation.withState("create:open", true));
                 block2.setPermutation(block2.permutation.withState("create:open", true));
             } else {
-                const entity = e.block.hasTag("upper_door")
-                  ? e.dimension
-                      .getEntitiesAtBlockLocation(block2.location)
-                      .filter((v) => v.typeId == e.block.type.id)[0]
-                  : e.dimension
-                      .getEntitiesAtBlockLocation(e.block.location)
-                      .filter((v) => v.typeId == e.block.type.id)[0];
+                const entity = e.block.hasTag("upper_door") ? e.dimension.getEntitiesAtBlockLocation(block2.location)[0] : e.dimension.getEntitiesAtBlockLocation(e.block.location)[0];
                 entity.setProperty("create:open", false);
                 system.runTimeout(() => {
                     if (entity.getProperty("create:open") === false) {
@@ -69,30 +57,31 @@ world.beforeEvents.worldInitialize.subscribe(initEvent => {
                 const nDirection = neighbour.permutation.getState("minecraft:cardinal_direction");
                 const nHinge = !neighbour.hasTag("left");
                 if ((nHinge != hinge) && (direction == nDirection)) {
-                    const upper = neighbour.hasTag("upper_door");
-                    const bottom = upper ? neighbour.below() : neighbour;
-                    const top = upper ? neighbour: neighbour.above();
-                    if (!bottom.hasTag("open")) {
-                        const entity2 = e.dimension.spawnEntity(e.block.typeId, bottom.bottomCenter());
-                        entity2.setProperty("create:cardinal_direction", getEntityDirection(nDirection));
-                        entity2.setProperty("create:hinge", nHinge);
-                        top.setPermutation(top.permutation.withState("create:open", true));
-                        bottom.setPermutation(bottom.permutation.withState("create:open", true));
-                    } else {
-                        const entity2 = e.dimension
-                          .getEntitiesAtBlockLocation(bottom.location)
-                          .filter((v) => v.typeId == e.block.type.id)[0];
-                        entity2.setProperty("create:open", false);
-                        system.runTimeout(() => {
-                            if (entity2.getProperty("create:open") === false) {
-                                top.setPermutation(top.permutation.withState("create:open", false));
-                                bottom.setPermutation(bottom.permutation.withState("create:open", false));
-                                world.playSound("close.iron_door", top.location);
-                                system.runTimeout(() => {
-                                    entity2.remove(); 
-                                },1);
-                            };
-                        }, 7);
+                    const nOpen = neighbour.hasTag("open");
+                    if (nOpen == open) {
+                        const upper = neighbour.hasTag("upper_door");
+                        const bottom = upper ? neighbour.below() : neighbour;
+                        const top = upper ? neighbour: neighbour.above();
+                        if (!open) {
+                            const entity2 = e.dimension.spawnEntity(e.block.typeId, bottom.bottomCenter());
+                            entity2.setProperty("create:cardinal_direction", getEntityDirection(nDirection));
+                            entity2.setProperty("create:hinge", nHinge);
+                            top.setPermutation(top.permutation.withState("create:open", true));
+                            bottom.setPermutation(bottom.permutation.withState("create:open", true));
+                        } else {
+                            const entity2 = e.dimension.getEntitiesAtBlockLocation(bottom.location)[0];
+                            entity2.setProperty("create:open", false);
+                            system.runTimeout(() => {
+                                if (entity2.getProperty("create:open") === false) {
+                                    top.setPermutation(top.permutation.withState("create:open", false));
+                                    bottom.setPermutation(bottom.permutation.withState("create:open", false));
+                                    world.playSound("close.iron_door", top.location);
+                                    system.runTimeout(() => {
+                                        entity2.remove(); 
+                                    },1);
+                                };
+                            }, 7);
+                        };
                     };
                 };
             };
@@ -122,5 +111,35 @@ function getN(string, location, bool) {
         case "north": 
             return {x: location.x - val, y: location.y, z: location.z};
         default: return {x: location.x + val, y: location.y, z: location.z};
+    }
+}
+
+function getHinge(player, direction) {
+    console.warn(direction);
+    switch(direction) {
+        case "north":
+            if (player.getViewDirection().x < 0) {
+                return "left";
+            } else {
+                return "right";
+            };
+        case "west":
+            if (player.getViewDirection().z > 0) {
+                return "left";
+            } else {
+                return "right";
+            };
+        case "east":
+            if (player.getViewDirection().z < 0) {
+                return "left";
+            } else {
+                return "right";
+            };
+        default:
+            if (player.getViewDirection().x > 0) {
+                return "left";
+            } else {
+                return "right";
+            };
     }
 }
